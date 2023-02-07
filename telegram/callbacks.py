ff0a4b77975bde45e50d376d96901c7fb17cbdf8
Rafael López <rlm96@hotmail.com>
2023-02-08 00:43:53 +0100

    Change video encoder, video files name and move temp videos to media folder

--- telegram/callbacks.py
+++ telegram/callbacks.py
@@ -6,12 +6,12 @@ def watchman_callback(context: CallbackContext):
         if(context.job.context.door_sensor.cached_status):
             try:
                 context.bot.send_message(chat_id=context.job.context.allowed_chat, text="The door has been opened \U0001F6AA\U0001F6A8")
-                video_name = context.job.context.rtsp_stream.record_video(10)
-                context.bot.send_video(chat_id=context.job.context.allowed_chat, video=open(video_name, 'rb'))
-                if(os.path.isfile(video_name)):
-                    os.remove(video_name)
+                video = context.job.context.rtsp_stream.record_video(10)
+                context.bot.send_video(chat_id=context.job.context.allowed_chat, video=open(video.path, 'rb'), supports_streaming=True, width=video.width, height=video.height)
+                if(os.path.isfile(video.path)):
+                    os.remove(video.path)
             except:
-                if(os.path.isfile(video_name)):
-                    os.remove(video_name)
+                if(os.path.isfile(video.path)):
+                    os.remove(video.path)
         else:
             context.bot.send_message(chat_id=context.job.context.allowed_chat, text="The door has been closed \U0001F6AA\U00002705")
--- telegram/rtsp.py
+++ telegram/rtsp.py
@@ -1,8 +1,10 @@
 import cv2
 import time
+from datetime import datetime
 
 base_address = "rtsp://{user}:{password}@{ip}/{path}"
-video_name = 'output.mp4'
+video_extension = '.mp4'
+video_base_path = "media/"
 
 class Stream:
     def __init__(self, ip, user, password, path):
@@ -14,8 +16,9 @@ class Stream:
         width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
         height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-        fourcc = cv2.VideoWriter_fourcc(*'mp4v')
-        writer = cv2.VideoWriter(video_name, fourcc, 20.0, (width,height))
+        fourcc = cv2.VideoWriter_fourcc(*'h264')
+        video_name = datetime.now().strftime("%d-%B-%Y %Hh%Mm%Ss") + video_extension
+        writer = cv2.VideoWriter(video_base_path + video_name, fourcc, 20.0, (width, height))
 
         timeout = time.time() + seconds_to_record
         while True:
@@ -27,4 +30,10 @@ class Stream:
         cap.release()
         writer.release()
 
-        return video_name
+        return Video(video_base_path + video_name, width, height)
+
+class Video:
+    def __init__(self, path, width, height):
+        self.path = path
+        self.width = width
+        self.height = height
