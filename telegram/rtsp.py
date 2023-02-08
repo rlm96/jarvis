ebe7ffa9c1b03958cc298b4f85a8a10f354f0dce
Rafael López <rlm96@hotmail.com>
2023-02-08 01:11:41 +0100

    Use os.path.join to get the video(s) path

--- telegram/rtsp.py
+++ telegram/rtsp.py
@@ -1,14 +1,17 @@
 import cv2
 import time
+import os
 from datetime import datetime
 
 base_address = "rtsp://{user}:{password}@{ip}/{path}"
 video_extension = '.mp4'
-video_base_path = "media/"
+video_base_path = 'media'
 
 class Stream:
     def __init__(self, ip, user, password, path):
         self.url_address = base_address.format(ip=ip, user=user, password=password, path=path)
+        if not os.path.exists(video_base_path):
+            os.makedirs(video_base_path)
 
     def record_video(self, seconds_to_record):
         cap = cv2.VideoCapture(self.url_address)
@@ -18,7 +21,8 @@ class Stream:
 
         fourcc = cv2.VideoWriter_fourcc(*'h264')
         video_name = datetime.now().strftime("%d-%B-%Y %Hh%Mm%Ss") + video_extension
-        writer = cv2.VideoWriter(video_base_path + video_name, fourcc, 20.0, (width, height))
+        full_video_path = os.path.join(video_base_path, video_name)
+        writer = cv2.VideoWriter(full_video_path, fourcc, 20.0, (width, height))
 
         timeout = time.time() + seconds_to_record
         while True:
@@ -30,7 +34,7 @@ class Stream:
         cap.release()
         writer.release()
 
-        return Video(video_base_path + video_name, width, height)
+        return Video(full_video_path, width, height)
 
 class Video:
     def __init__(self, path, width, height):
