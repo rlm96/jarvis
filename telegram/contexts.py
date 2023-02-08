72059de0b9c2de7dde751f4eae9984b1dcc6c9c8
Rafael López <rlm96@hotmail.com>
2023-02-08 01:28:17 +0100

    Use sensor_true_on_open to consider (sensor =) false as a value for opened door

--- telegram/contexts.py
+++ telegram/contexts.py
@@ -3,7 +3,8 @@ class BaseContext:
         pass
 
 class WatchmanContext(BaseContext):
-    def __init__(self, allowed_chat, door_sensor, rtsp_stream):
+    def __init__(self, allowed_chat, door_sensor, sensor_true_on_open, rtsp_stream):
         self.allowed_chat = allowed_chat
         self.door_sensor = door_sensor
+        self.sensor_true_on_open = sensor_true_on_open
         self.rtsp_stream = rtsp_stream
