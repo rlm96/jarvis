989ac0c7cb725a90f4808aa0b61f63494c3b238c
Rafael López <rlm96@hotmail.com>
2023-02-25 22:15:55 +0100

    Drain socket when a door event is processed

--- watchman/src/main.py
+++ watchman/src/main.py
@@ -5,6 +5,7 @@ import logging
 import socket
 import rtsp
 import asyncio
+import select
 
 logging.basicConfig(
     level = logging.ERROR, format = "%(asctime)s %(message)s"
@@ -26,11 +27,20 @@ allowed_chat = int(os.getenv('ALLOWED_CHAT_ID'))
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((udp_ip, udp_port))
+sock.connect(sensor_ip)
+
+def drain_socket():
+    """Drain/empty any pending incoming data present in the socket"""
+    input = [sock]
+    while True:
+        inputready, o, e = select.select(input,[],[], 0.0)
+        if len(inputready)==0: break
+        for s in inputready: s.recv(1024)
 
 async def process_event_async():
     try:
-        await application.bot.send_message(chat_id=allowed_chat, text="The door sensor has recorded a new event \U0001F6AA\U0001F6A8")
         video = rtsp_stream.record_video(10)
+        await application.bot.send_message(chat_id=allowed_chat, text="The door sensor has recorded a new event \U0001F6AA\U0001F6A8")
         await application.bot.send_video(chat_id=allowed_chat, video=open(video.path, 'rb'), supports_streaming=True, width=video.width, height=video.height)
         if(os.path.isfile(video.path)):
             os.remove(video.path)
@@ -43,6 +53,7 @@ async def main():
         data, addr = sock.recvfrom(1024)
         if addr[0] == sensor_ip:
             await process_event_async()
+            drain_socket()
 
 if __name__ == '__main__':
     asyncio.run(main())
