2aaf1b38ca737c49bbf0e16b4c56a64e8109a9eb
Rafael López <rlm96@hotmail.com>
2023-02-25 22:51:09 +0100

    Update run_bot.sh

--- watchman/run_bot.sh
+++ watchman/run_bot.sh
@@ -1,2 +1,2 @@
 docker build -t jarvis-watchman .
-docker run -p 6667:6667/udp --name jarvis-watchman -d jarvis-watchman
+docker run -p 6667:6667 -p 6667:6667/udp --name jarvis-watchman -d jarvis-watchman
