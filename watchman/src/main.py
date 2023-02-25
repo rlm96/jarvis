b125b7bfcad95b2bb3dd6858f06f0a9d148a8f13
Rafael López <rlm96@hotmail.com>
2023-02-25 17:28:09 +0100

    Use asyncio to process events in watchman

--- watchman/src/main.py
+++ watchman/src/main.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import logging
 import socket
 import rtsp
+import asyncio
 
 logging.basicConfig(
     level = logging.ERROR, format = "%(asctime)s %(message)s"
@@ -13,7 +14,7 @@ logger = logging.getLogger()
 load_dotenv()
 
 udp_ip = ''
-udp_port = os.getenv('UDP_PORT')
+udp_port = int(os.getenv('UDP_PORT'))
 
 sensor_ip = os.getenv('CONTACT_SENSOR_IP')
 
@@ -26,9 +27,9 @@ allowed_chat = int(os.getenv('ALLOWED_CHAT_ID'))
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((udp_ip, udp_port))
 
-async def new_method():
+async def process_event_async():
     try:
-        await application.bot.send_message(chat_id=allowed_chat, text="The door has been opened \U0001F6AA\U0001F6A8")
+        await application.bot.send_message(chat_id=allowed_chat, text="The door sensor has recorded a new event \U0001F6AA\U0001F6A8")
         video = rtsp_stream.record_video(10)
         await application.bot.send_video(chat_id=allowed_chat, video=open(video.path, 'rb'), supports_streaming=True, width=video.width, height=video.height)
         if(os.path.isfile(video.path)):
@@ -37,7 +38,11 @@ async def new_method():
         if(os.path.isfile(video.path)):
             os.remove(video.path)
 
-while True:
-    data, addr = sock.recvfrom(1024)
-    if addr[0] == sensor_ip:
-        new_method()
+async def main():
+    while True:
+        data, addr = sock.recvfrom(1024)
+        if addr[0] == sensor_ip:
+            await process_event_async()
+
+if __name__ == '__main__':
+    asyncio.run(main())
