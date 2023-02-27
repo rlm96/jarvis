c5233cc4eda0266274510079e04f68b47bdccde8
Rafael López <rlm96@hotmail.com>
2023-02-27 01:00:37 +0100

    Add reminder message if bot is rebooted

--- telegram/src/main.py
+++ telegram/src/main.py
@@ -5,6 +5,7 @@ import logging
 import commands
 import callbacks
 import callbacks.datamodels as datamodels
+import asyncio
 
 logging.basicConfig(
     level = logging.ERROR, format = "%(asctime)s %(message)s"
@@ -19,4 +20,12 @@ allowed_chat = int(os.getenv('ALLOWED_CHAT_ID'))
 
 application.add_handler(CommandHandler('health', commands.health_command))
 
-application.run_polling()
+async def initialize_async():
+    await application.bot.send_message(chat_id=allowed_chat, 
+        text="J.A.R.V.I.S. has been potentially rebooted. Check if everything is working as expected and devices (e.g., WiFi plugs and sensors) are in the required state (on/off)")
+
+if __name__ == '__main__':
+    # run initialize_async() in the "background"
+    # submit the coroutine to the event loop as a task before start bot polling
+    asyncio.get_event_loop().create_task(initialize_async())
+    application.run_polling()
